0b8bcb8725a77ae15d09e7334f37f6e8eebd617a
pablosimkievich <pablosimkievich@gmail.com>
2024-01-21 02:09:44 -0300

    ShoppingCart header with cart icon y red box

--- src/components/ShoppingCart/Item.jsx
+++ src/components/ShoppingCart/Item.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import './Item.css'
 
 export const Item = () => {
   return (
--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,14 +1,40 @@
-import { Item } from './Item.jsx'
-
+import { Item } from "./Item.jsx"
+import "./ShoppingCart.css"
+import { Link, useNavigate } from 'react-router-dom'
+import { CiHome } from "react-icons/ci"
+import { CiShoppingCart } from "react-icons/ci"
+import { useCartStore } from "../../store/cartStore.jsx"
 
 export const ShoppingCart = () => {
 
+  const navigate = useNavigate()
+
+  const toyUnitsQ = useCartStore(state => state.toyUnitsQ)
+
   return (
     <div>
-      Soy Shopping Cart
-      < Item />
+      <div className="the-header-container">
+        <div className="detail-title-icon">
+          <h1 className="detail-title">
+            Rayuela Shopping Cart
+            <Link to="/cart">
+              <CiShoppingCart className="detail-icon" />
+            </Link>
+          </h1>
+          <div className="red-box-detail">{toyUnitsQ}</div>
+        </div>
+
+        <button className="btn home-btn" onClick={() => navigate("/")}>
+          Volver al Inicio
+          <CiHome className="home-icon" />
+        </button>
+      </div>
+
+      
+
+      <Item />
+
+
     </div>
-  )
-}
-
-
+  );
+};
